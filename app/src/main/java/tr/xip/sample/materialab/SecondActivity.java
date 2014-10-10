31955bb6c8ab562c6a4fbbe694b395707e33dda4
İhsan Işık <x.ihsan@gmail.com>
2014-10-10 14:42:50 +0200

    Initial commit

---
+++ app/src/main/java/tr/xip/sample/materialab/SecondActivity.java
@@ -0,0 +1,16 @@
+package tr.xip.sample.materialab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hikari on 10/10/14.
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
